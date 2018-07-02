d655a8a6baa05cd5872f9cb5475cf1d370d28ff9
Raphael Varonos <r.varonos@agneta.io>
2018-07-02 14:51:18 +0200

    Transfered from agneta platform to support other frontend packages

---
+++ .eslintrc.js
@@ -0,0 +1,14 @@
+module.exports = {
+  "globals": {
+    angular: true,
+    agneta: true,
+    require: true,
+    $$template: true,
+    module: true
+  },
+  "env": {
+    "es6": false,
+    "browser": true,
+    "node": false
+  }
+}
